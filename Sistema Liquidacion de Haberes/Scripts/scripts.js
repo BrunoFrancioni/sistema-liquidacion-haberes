57957e604c95538937180d3f816eae4fc7053034
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-03-21 17:18:59 -0300

    Eliminar código de JavaScript dentro de la vista de Crear empleado, y colocarlos en su respectivo archivo scripts.js.

--- Sistema Liquidacion de Haberes/Scripts/scripts.js
+++ Sistema Liquidacion de Haberes/Scripts/scripts.js
@@ -1,2 +1,93 @@
 ﻿
+$(function () {
+    'use strict';
 
+    const sectores = {
+        maestranza: 1,
+        administrativo: 2,
+        cajero: 3,
+        personal: 4,
+        auxiliar: 4,
+        vendedor: 6
+    };
+
+    $('input#maestranza').on('click', function () {
+        $('#categorias').removeData();
+
+        var url = "/Home/ObtenerCategorias";
+        var data = { idSector: sectores.maestranza };
+
+        insertarCategorias(url, data);
+
+    })
+
+    $('input#administrativo').on('click', function () {
+        quitarChecked();
+
+        $('select#categorias').remove();
+
+        var url = "/Home/ObtenerCategorias";
+        var data = { idSector: sectores.administrativo };
+
+        insertarCategorias(url, data);
+    })
+
+    $('input#cajero').on('click', function () {
+        quitarChecked();
+
+        $('select#categorias').remove();
+
+        var url = "/Home/ObtenerCategorias";
+        var data = { idSector: sectores.cajero };
+
+        insertarCategorias(url, data);
+    })
+
+    $('input#personal').on('click', function () {
+        quitarChecked();
+
+        $('select#categorias').remove();
+
+        var url = "/Home/ObtenerCategorias";
+        var data = { idSector: sectores.personal };
+
+        insertarCategorias(url, data);
+    })
+
+    $('input#auxiliar').on('click', function () {
+        quitarChecked();
+
+        $('select#categorias').remove();
+
+        var url = "/Home/ObtenerCategorias";
+        var data = { idSector: sectores.auxiliar };
+
+        insertarCategorias(url, data);
+    })
+
+    $('input#vendedor').on('click', function () {
+        quitarChecked();
+
+        $('select#categorias').remove();
+
+        var url = "/Home/ObtenerCategorias";
+        var data = { idSector: sectores.vendedor };
+
+        insertarCategorias(url, data);
+    })
+
+
+    function quitarChecked() {
+        $('input#maestranza').removeAttr("checked");
+
+        return false;
+    }
+
+    function insertarCategorias(url, data) {
+        $.get(url, data).done(function (data) {
+            $('div#lista-categorias').append(data);
+        }).fail(function () {
+            alert("Ha fallado");
+        });
+    }
+})
